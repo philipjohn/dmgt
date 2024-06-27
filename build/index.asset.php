1d2cb9c2b93da1485c17dc12448432a0264a5619
Philip John <phil@philipjohn.me.uk>
2024-06-27 13:25:25 +0100

    Update build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '24afc32eeaffbd4e4378');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9f483f6b3ed31e3060c0');
