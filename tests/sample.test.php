084403a490cf662911bd44fac460f98762380306
Philip John <phil@philipjohn.me.uk>
2024-07-02 13:03:21 +0100

    Tests: Fix loading of the PHP tests

--- tests/sample.test.php
+++ tests/sample.test.php
@@ -8,12 +8,12 @@
 /**
  * Sample test case.
  */
-class TestSample extends WP_UnitTestCase {
+class Test_Sample extends WP_UnitTestCase {
 
 	/**
 	 * A single example test.
 	 */
-	public function test_sample() {
+	public function testsample() {
 		// Replace this with some actual testing code.
 		$this->assertTrue( true );
 	}
